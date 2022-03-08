fed1ac19416e3bd25fc5f039b6ba7c6252de4d57
geekyNischal <nischaln@geekyants.com>
2022-03-09 01:17:15 +0530

    Setup code.

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import "./App.css";
+
+export const App: React.FC<{}> = () => {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>Task Management</p>
+      </header>
+    </div>
+  );
+};
+
+export default App;
